ab01e0a3dd6d87b019b5ceb27bdd0e463325ecfa
WadikarVaibhav <vaibhavwadikar8@gmail.com>
2019-04-26 17:26:26 -0700

    Created layout for car search

---
+++ app/src/main/java/com/android/carrental/CarSearchActivity.java
@@ -0,0 +1,13 @@
+package com.android.carrental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_search);
+    }
+}
